162570a8099600b0261cf645498f603b520fd6e1
martu-giaco <martina.giacomini@davinci.edu.ar>
2025-11-13 10:52:26 -0300

    footer updateado y sección de contacto terminada

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -19,35 +19,57 @@ const sendMessage = () => {
 </script>
 
 <template>
-  <div class="justify-center bg-no-repeat bg-cover w-full flex flex-col items-center bg-[url(assets/imgs/header-bg.png)] pt-60 pb-40 bg-gradiente">
-    <h1 class="text-5xl text-[#306067]">Contacto</h1>
-    <h2 class="text-2xl text-[#37A0AF]">Escribínos tus dudas o sugerencias</h2>
+  <section class="justify-center bg-no-repeat bg-cover w-full items-center bg-[url(assets/imgs/header-bg.png)] pt-50 pb-60 bg-gradiente text-center">
 
-    <div class="container justify-center items-center">
-      <div class="flex flex-col gap-4 mt-6 w-full px-20">
+    <div class="container flex flex-row justify-center items-center mx-auto">
+      <img src="../assets/imgs/contacto-img.png" alt="imagen decorativa" class="w-xl h-auto object-contain me-20">
+      <div class="w-full">
+        <h1 class="text-5xl text-[#306067]">Contacto</h1>
+    <h2 class="text-2xl text-[#37A0AF] mb-10">Escribínos tus dudas o sugerencias</h2>
+        <div>
+          <div class="flex flex-col w-full">
       <input
         v-model="name"
         type="text"
         placeholder="Nombre"
-        class="p-5 border bg-white/40 backdrop-blur-lg rounded-xl glass focus:outline-none focus:ring-2 focus:ring-[#37A0AF]"
+        class="text-[#37A0AF] h-15 p-5 mt-5 border bg-white/40 backdrop-blur-lg rounded-xl glass focus:outline-none focus:ring-2 focus:ring-[#37A0AF]"
       />
       <input
         v-model="email"
         type="email"
         placeholder="Correo electrónico"
-        class="p-5 border bg-white/40 backdrop-blur-lg rounded-xl glass focus:outline-none focus:ring-2 focus:ring-[#37A0AF]"
+        class="text-[#37A0AF] h-15 p-5 mt-5 border bg-white/40 backdrop-blur-lg rounded-xl glass focus:outline-none focus:ring-2 focus:ring-[#37A0AF]"
       />
       <textarea
         v-model="message"
         rows="5"
         placeholder="Mensaje"
-        class="p-5 border bg-white/40 backdrop-blur-lg rounded-xl glass focus:outline-none focus:ring-2 focus:ring-[#37A0AF]"
+        class="text-[#37A0AF] h-30 p-5 mt-5 border bg-white/40 backdrop-blur-lg rounded-xl glass focus:outline-none focus:ring-2 focus:ring-[#37A0AF]"
       ></textarea>
     </div>
 
     <div class="mt-6 flex justify-center">
       <a @click="sendMessage" class="text-xl px-20 py-3 pt-4 bg-[#306067] text-white rounded-2xl shadow-md boton-header">Enviar Mensaje</a>
     </div>
+        </div>
+        <div class="flex items-center justify-between mt-10">
+          <div class="text-start">
+            <h3 class="text-[#306067]">Email</h3>
+            <p>kälm@email.com</p>
+          </div>
+          <div class="text-start">
+            <h3 class="text-[#306067]">Basadas en</h3>
+            <p>Buenos Aires, Argentina.</p>
+          </div>
+          <div class="align-end">
+            <ul class="flex gap-3 h-5">
+              <li class="contents"><a href="https://www.instagram.com/kalm_oficial/"  target=”_blank” class="bg-[url(assets/imgs/insta.png)] bg-no-repeat bg-contain w-5" style="font-size: 0;">Instagram</a></li>
+              <li class="contents"><a href="https://www.instagram.com/kalm_oficial/"  target=”_blank” class="bg-[url(assets/imgs/tiktok.png)] bg-no-repeat bg-contain w-5" style="font-size: 0;">Tik Tok</a></li>
+              <li class="contents"><a href="https://www.instagram.com/kalm_oficial/"  target=”_blank” class="bg-[url(assets/imgs/pinterest.png)] bg-no-repeat bg-contain w-5" style="font-size: 0;">Pinterest</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
