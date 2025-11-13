fcd522aa671f9a5433357c813c8f7f99aa221974
martu-giaco <martina.giacomini@davinci.edu.ar>
2025-11-12 22:34:45 -0300

    sección equipo hecha

--- src/views/Equipo.vue
+++ src/views/Equipo.vue
@@ -5,22 +5,19 @@ const integrantes = [
     id: 1,
     name: 'Álvarez Martina',
     rol: 'Co-fundadora',
-    description: 'Directora del Departamento de Marketing',
-    foto: '/*aca ponemos un dibujito??*/'
+    description: 'Directora del Departamento de Marketing'
   },
   {
     id: 2,
     name: 'Barrientos Abril',
     rol: 'Co-fundadora',
-    description: 'Directora del Departamento de Tecnología',
-    foto: '/*aca ponemos un dibujito??*/'
+    description: 'Directora del Departamento de Tecnología'
   },
   {
     id: 3,
     name: 'Giacomini Martina',
     rol: 'Co-fundadora',
-    descripcion: 'Directora del Departamento de Diseño',
-    foto: '/*aca ponemos un dibujito??*/'
+    description: 'Directora del Departamento de Diseño'
   }
 ];
 </script>
@@ -30,13 +27,13 @@ const integrantes = [
         <h1 class="text-5xl text-[#306067]">Conocé a nuestro equipo</h1>
         <h2 class=" text-center text-2xl text-[#37A0AF] mb-5 ">Las personas que hacen a <i>Kälm</i>, <i>Kälm</i>.</h2>
 
-        <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-6 mt-3 items-stretch container mx-auto">
-            <div v-for="integrante in integrantes" :key="integrante.id" class="glass">
-                <img src="{{ integrante.foto }}" alt="ilustración integrante">
+        <div class="flex flex-wrap gap-10 mt-3 items-center justify-center container mx-auto sm:grid-cols-1 md:grid-cols-3">
+            <div v-for="integrante in integrantes" :key="integrante.id" class="glass px-4 py-30 rounded-xl flex flex-col w-100 items-center text-center">
+                <img src="../assets/imgs/perfil.png" alt="ilustración perfil" class="h-40">
                 <div>
-                    <h3>{{ integrante.name }}</h3>
-                    <h4 class="text-text-[#37A0AF]">{{ integrante.rol }}</h4>
-                    <p>{{ integrante.descripcion }}</p>
+                    <h3 class="text-[#306067] text-2xl mt-3">{{ integrante.name }}</h3>
+                    <h4 class="text-[#37A0AF] mb-1 ">{{ integrante.rol }}</h4>
+                    <p>{{ integrante.description }}</p>
                 </div>
             </div>
         </div>
