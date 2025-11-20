1b0a2cdf8a6c963fb8b9ef267b223f24e696ec7a
martu-giaco <martina.giacomini@davinci.edu.ar>
2025-11-19 23:59:59 -0300

    empleos partecita y about arreglado

--- src/views/Empleos.vue
+++ src/views/Empleos.vue
@@ -1,3 +1,6 @@
 <template>
-    
+    <section class="flex flex-col items-center w-full justify-center bg-no-repeat bg-cover bg-[url(assets/imgs/header-bg.png)] pt-50 pb-60 bg-gradiente">
+        <h1 class="text-5xl text-[#306067]">Conocé a nuestro equipo</h1>
+        <h2 class=" text-center text-2xl text-[#37A0AF] mb-5 ">Las personas que hacen a <i>Kälm</i>, <i>Kälm</i>.</h2>
+    </section>
 </template>
