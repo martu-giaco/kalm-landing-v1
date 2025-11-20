c7338c9e21c1a40b2f04ee361f511a5c5d0b426a
martu-giaco <martina.giacomini@davinci.edu.ar>
2025-11-20 00:00:16 -0300

    eso

--- src/views/About.vue
+++ src/views/About.vue
@@ -11,7 +11,7 @@
         <h1 class="text-5xl text-[#306067] mb-2">Sobre <i class="logo-text">Kälm</i></h1>
 
         <!-- Subtítulo general -->
-        <p class="mb-5">En el Contacto de <i>Kälm</i> nos enfocamos en brindar apoyo y recursos para el bienestar integral de las personas de manera gratuita, promoviendo el amor propio, la paciencia y la autocompasión.</p>
+        <p class="mb-5">En el equipo de <i>Kälm</i> nos enfocamos en brindar apoyo y recursos para el bienestar integral de las personas de manera gratuita, promoviendo el amor propio, la paciencia y la autocompasión.</p>
 
         <p class="mb-5">Creemos que el bienestar es un viaje personal, único y escencial para todos. Nuestro propósito es acompañarte con cuidado, transparencia y amor, ofreciéndote herramientas gratuitas que te ayuden a reconectar con vos mism@ y con lo que realmente necesitás. Cada rutina, cada recordatorio, cada momento de pausa es una invitación a volver a vos. Porque el autocuidado no se trata de perfección, sino de aprender a mirarte con paciencia, a cuidarte con ternura y a celebrar cada pequeño paso en tu propio ritmo.</p>
 
